932d9a6a84452e4e62dff635c6167bd7aaab3e58
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-07 19:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function currentLine(katzDeli) {
      return "The line is currently empty.";
    }
    else
-     return ("The line is currently: " + line);
+     return ("The line is currently:" + line);
  
 }
